362392d2d4f42cb5c75074968115e7979a96681e
webmaxvianna <webmaxvianna@gmail.com>
2020-08-25 10:39:55 -0300

    Redirecionar usuário para área restrita

--- config/routes.php
+++ config/routes.php
@@ -51,7 +51,8 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, templates/Pages/home.php)...
      */
-    $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    // $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $builder->connect('/', ['controller' => 'Pages', 'action' => 'index']);
 
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
