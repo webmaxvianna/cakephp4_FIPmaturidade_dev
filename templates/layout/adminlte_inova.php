1fc46184a04a769d390d62bffef4112db9ec8cf9
webmaxvianna <webmaxvianna@gmail.com>
2020-08-27 12:11:36 -0300

    Novo arquivo CSS

--- templates/layout/adminlte_inova.php
+++ templates/layout/adminlte_inova.php
@@ -43,6 +43,11 @@
       '/adminlte/dist/css/ionicons.min'
     ]) 
   ?>
+  <?= 
+    $this->Html->css([
+      'custom-style'
+    ]) 
+  ?>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
