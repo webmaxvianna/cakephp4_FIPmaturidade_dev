cfbf247779979539bd41b2eac9e96d97baf676d3
andrehgdias <andredias.us@gmail.com.com>
2020-08-28 09:23:38 -0300

    fix: Funções no controller de Ideas relativo ao Candidato restauradas

--- src/Controller/Admin/IdeasController.php
+++ src/Controller/Admin/IdeasController.php
@@ -177,7 +177,71 @@ class IdeasController extends AppController
         $this->set(compact('idea'));
         $this->set(compact('user'));
     }
-    
+
+    public function listIdeas()
+    {
+        $filteredIdeas = $this->Ideas->find('all', [
+            'contain' => [
+                'Owners'
+            ],
+            'conditions' => [
+                'user_id =' => $this->Auth->user('id')
+            ]
+        ]);
+
+        $ideas = $this->paginate($filteredIdeas);
+        $edicts = $this->Ideas->Edicts->find('all', ['limit' => 200]);
+        $this->set(compact('ideas', 'edicts'));
+    }
+
+    public function editSumario($id = null)
+    {
+        $idea = $this->Ideas->get($id, [
+            'contain' => ['Users'],
+        ]);
+
+        if ($idea['user_id'] != $this->Auth->user('id')) {
+            $this->Flash->error(__('Access Denied.'));
+
+            return $this->redirect(['action' => 'listIdeas', $this->Auth->user('id')]);
+        }
+
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $idea = $this->Ideas->patchEntity($idea, $this->request->getData());
+            if ($this->Ideas->save($idea)) {
+                $this->Flash->success(__('The idea has been saved.'));
+
+                return $this->redirect(['action' => 'listIdeas']);
+            }
+            $this->Flash->error(__('The idea could not be saved. Please, try again.'));
+        }
+        $this->set(compact('idea'));
+    }
+
+    public function editCanvas($id = null)
+    {
+        $idea = $this->Ideas->get($id, [
+            'contain' => ['Users'],
+        ]);
+
+        if ($idea['user_id'] != $this->Auth->user('id')) {
+            $this->Flash->error(__('Access Denied.'));
+
+            return $this->redirect(['action' => 'listIdeas', $this->Auth->user('id')]);
+        }
+
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $idea = $this->Ideas->patchEntity($idea, $this->request->getData());
+            if ($this->Ideas->save($idea)) {
+                $this->Flash->success(__('The idea has been saved.'));
+
+                return $this->redirect(['action' => 'listIdeas']);
+            }
+            $this->Flash->error(__('The idea could not be saved. Please, try again.'));
+        }
+        $this->set(compact('idea'));
+    }
+
     public function indexCandidatos($id = null)
     {
         $this->paginate = [
