8c8fd8e5ebba136d8c28cbfd08fa0ffb22e4ce57
webmaxvianna <webmaxvianna@gmail.com>
2020-09-09 15:06:05 -0300

    Personalização do index da Dashboard

--- templates/Admin/Dashboards/index.php
+++ templates/Admin/Dashboards/index.php
@@ -7,9 +7,40 @@
               <!-- /.card-header -->
               <div class="card-body">
                 <h3>Olá, <?= $userLogged->nome_completo ?>!</h3>
-                <p class="lead">Seja bem-vindo(a) ao Sistema de Maturidade.</p>
+                <?php
+                  switch ($userLogged->sexo) {
+                    case 'Masculino':
+                      $sexo = 'o';
+                      break;
+                    case 'Feminino':
+                      $sexo = 'a';
+                      break;
+                    default:
+                      $sexo = 'o(a)';
+                      break;
+                  }
+                ?>
+                <p class="lead">Seja bem-vind<?= $sexo ?> ao Sistema de Maturidade.</p>
                 <p class="lead">
-                  Aqui você pode cadastrar sua ideia de negócio e consultar os editais publicados.
+                <?php
+                  switch ($userLogged->role->funcao) {
+                    case 'Candidato':
+                      echo 'Aqui você pode cadastrar sua ideia de negócio e consultar os editais publicados.';
+                      break;
+                    case 'Gestor':
+                      echo 'Aqui você pode ver todos os usuários cadastrados, publicar editais e ter controle total das ideias e atividades.';
+                      break;
+                    case 'Avaliador':
+                      echo 'Texto para avaliador';
+                      break;
+                    case 'Consultor':
+                      echo 'Texto para consultor';
+                      break;
+                    case 'Jurado':
+                      echo 'Texto para jurado';
+                      break;
+                  }
+                ?>
                 </p>
               </div>
               <!-- /.card-body -->
@@ -20,8 +51,10 @@
                 <li><?= ($userLogged->confirmacao_email == 1) ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Confirme seu endereço de email</li>
                 <li><?= $dados_pessoais ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Complete o preenchimento dos seus dados pessoais</li>
                 <li><?= $userLogged->foto ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Insira uma foto de perfil</li>
-                <li><?= $documentos ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Envie seus comprovantes de documentos</li>
-                <li><?= $ideia ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Crie e preencha todos os campos referentes a sua ideia</li>
+                <?php if ($userLogged->role->funcao == 'Candidato') : ?>
+                  <li><?= $documentos ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Envie seus comprovantes de documentos</li>
+                  <li><?= $ideia ? '<i class="text-success fas fa-check-square"></i>&nbsp;&nbsp;' : '<i class="text-default far fa-square"></i>&nbsp;&nbsp;' ?>Crie e preencha todos os campos referentes a sua ideia</li>
+                <?php endif; ?>
               </ul>
             </div>
             <!-- /.card -->
