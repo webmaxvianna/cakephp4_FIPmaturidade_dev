aada51306026de0337bc31981770981c57280c9e
webmaxvianna <webmaxvianna@gmail.com>
2020-09-10 12:14:22 -0300

    Novo Layout para vincular usuários

--- templates/Admin/Specialties/vincular_jurados.php
+++ templates/Admin/Specialties/vincular_jurados.php
@@ -1,52 +1,66 @@
 <!-- Breadcrumbs -->
 <?php
     $this->Breadcrumbs->add([
+        ['title' => 'Início', 'url' => ['controller' => 'dashboards', 'action' => 'index']],
         ['title' => 'Especialidades', 'url' => ['controller' => 'specialties', 'action' => 'index']],
-        ['title' => 'Vincular-Desvincular Consultores']
+        ['title' =>'Vincular Jurados']
     ]);
 ?>
 <!-- /.Breadcrumbs -->
 
+<!-- Main content -->
+<!-- Main content -->
 <section class="content">
     <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title"><?= $specialty->especialidade ?></h3>
-                <?= $this->Form->create($specialty) ?>
-              </div>
-              <div class="card-body">
-              <table class="table table-bordered table-striped">
-              <thead>
-                  <tr>
-                    <th>Jurados:</th>
-                  </tr>
-               </thead>
-               <tbody>
-                <tr>
-                    <th>
-                    <fieldset>
-                    <?php                    
-                        echo $this->Form->control('users._ids', [
-                            'options' => $jurados,
-                            'type' => 'select',
-                            'multiple' => 'checkbox',
-                            'label' => false
-                            ]);
+            <!-- column -->
+            <div class="col-md-8 offset-md-2">
+                <!-- general form elements disabled -->
+                <div class="card card-secondary">
+                    <div class="card-header cursor-pointer"data-toggle="collapse" href="#card3">
+                        <h3 class="card-title">Vincular ou Desvincular Jurados</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <?php  
+                        $myTemplates = [
+                            'checkboxWrapper' => '<div class="form-check">{{label}}</div>',
+                            'nestingLabel' => '{{hidden}}{{input}}<label class="form-check-label">{{text}}</label>',
+                            'error' => '<div class="error invalid-feedback">{{content}}</div>',
+                        ];
+                        $this->Form->setTemplates($myTemplates);
+                        $this->Form->setConfig('errorClass', 'is-invalid');
                     ?>
-                    </fieldset>
-                    </th>
-                </tr>
-                <tr>
-                    <th><?= $this->Form->button(__('Alterar'),['class'=>'btn btn-primary']) ?></th>
-                </tr>
-                </tbody>
-              </table>
-              </div>
-              </div>
+                    <?= $this->Form->create($specialty) ?>
+                    <div class="card-body collapse show" id="card1">
+                        <div>
+                            <h2><?= $specialty->titulo ?></h2>
+                            <p class="lead">Lista de jurados:</p>
+                        </div>
+                        <div class="form-group">
+                        <?php                    
+                            echo $this->Form->control('users._ids', [
+                                'options' => $jurados,
+                                'class' => 'form-check-input',
+                                'type' => 'select',
+                                'multiple' => 'checkbox',
+                                'label' => false
+                                ]);
+                        ?>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <?= $this->Form->button(__('Salvar vínculos'), ['class' => 'btn btn-primary col-md-6 offset-md-3']) ?>
+                    </div>
+                    <?= $this->Form->end() ?>
+                </div>
+                <!-- /.card -->
+
             </div>
-          </div>
+            <!--/.col -->
         </div>
-    </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
 </section>
+<!-- /.content -->
