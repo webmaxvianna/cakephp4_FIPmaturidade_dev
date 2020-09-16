cc805d4e9ec8db905974a89e329333f045d870f1
webmaxvianna <webmaxvianna@gmail.com>
2020-09-16 08:24:05 -0300

    Controle de erros do sistema

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -1,42 +1,20 @@
-<?php
-/**
- * @var \App\View\AppView $this
- */
-use Cake\Core\Configure;
-use Cake\Error\Debugger;
+<section class="content">
+    <div class="error-page mt-5 text-center">
+        <h2 class="headline text-danger mr-3">Error</h2>
+    
+        <div class="error-content">
+            <h3><i class="fas fa-exclamation-triangle text-danger"></i> Oops! Aconteceu algo de errado.</h3>
 
-$this->layout = 'error';
+            <p>
+            Por favor, tente acessar novamente o sistema inserindo seu nome de usuário e sua senha.
+            </p>
 
-if (Configure::read('debug')) :
-    $this->layout = 'dev_error';
+        </div>
+    </div>
 
-    $this->assign('title', $message);
-    $this->assign('templateName', 'error500.php');
-
-    $this->start('file');
-?>
-<?php if (!empty($error->queryString)) : ?>
-    <p class="notice">
-        <strong>SQL Query: </strong>
-        <?= h($error->queryString) ?>
-    </p>
-<?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-    <strong>SQL Query Params: </strong>
-    <?php Debugger::dump($error->params) ?>
-<?php endif; ?>
-<?php if ($error instanceof Error) : ?>
-    <strong>Error in: </strong>
-    <?= sprintf('%s, line %s', str_replace(ROOT, 'ROOT', $error->getFile()), $error->getLine()) ?>
-<?php endif; ?>
-<?php
-    echo $this->element('auto_table_warning');
-
-    $this->end();
-endif;
-?>
-<h2><?= __d('cake', 'An Internal Error Has Occurred') ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= h($message) ?>
-</p>
+    <div class="error-page mt-5 text-center">
+        <p>
+            <a href="<?= $this->Url->build('/admin/') ?>">Retornar ao sistema</a>
+        </p>
+    </div>
+</section>
